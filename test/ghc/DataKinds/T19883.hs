a48ff6ca9565b8004cf44fe05bfd031bed57cd8a
Mario Blažević <blamario@protonmail.com>
2022-03-05 23:04:44 -0500

    Another test made to pass

---
+++ test/ghc/DataKinds/T19883.hs
@@ -0,0 +1,18 @@
+{-# Language DataKinds #-}
+{-# Language ExplicitForAll, KindSignatures, PolyKinds #-}
+{-# Language NoImplicitPrelude #-}
+{-# Language RebindableSyntax #-}
+
+module T19883 where
+
+import GHC.Types
+import qualified Prelude
+
+class Eq (a :: TYPE r) where
+  (==) :: a -> a -> Bool
+
+class Num (a :: TYPE r) where
+  fromInteger :: Prelude.Integer -> a
+
+roundDef :: forall (a :: TYPE IntRep) . (Eq a, Num a) => a -> ()
+roundDef 0 = ()
