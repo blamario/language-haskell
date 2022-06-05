07c7239e6c1c343084d17ed81ea56938b75bf330
Mario Blažević <blamario@protonmail.com>
2022-06-04 23:17:09 -0400

    Positive ImpredicativeTypes tests

---
+++ test/ghc/ImpredicativeTypes/tc214.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ImpredicativeTypes, GADTs #-}
+{-# OPTIONS_GHC -fno-warn-deprecated-flags #-}
+
+-- This program sent GHC 6.6 into a loop, because the fixpointing
+-- of the substitution in type refinement got its in-scope-set
+-- from the answer!
+
+module ShouldCompile where
+
+------------------
+data Foo a b where F :: a -> Foo () a
+
+bar :: Foo () (forall a.a) -> ()
+bar (F _) = ()
+
+------------------
+data Foo2 a where F2 :: a -> Foo2 [a]
+
+bar2 :: Foo2 [forall a.a] -> ()
+bar2 (F2 _) = ()
