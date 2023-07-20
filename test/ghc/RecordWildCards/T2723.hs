741d58b883c82479e6dde69c8c99c41317955667
Mario Blažević <blamario@protonmail.com>
2023-07-20 11:21:34 -0400

    Positive ghc/RecordWildCards tests

---
+++ test/ghc/RecordWildCards/T2723.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+module WildCard where
+
+data Record = Record {field1 :: Int, field2 :: Double}
+
+field3 :: Int
+field3 = 3
+
+test1 (Record {field1, field2}) = let test = 1 in field1
+
+test2 :: (Record, Int)
+test2 = let
+    field1 = 10
+    field2 = 10.0
+    field3 = 8
+    in (Record {..}, field3)
