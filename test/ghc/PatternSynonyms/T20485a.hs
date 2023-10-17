ebacbd68e4b0ec74a4c7af24c5b13d3374669b7b
Mario Blažević <blamario@protonmail.com>
2023-10-17 12:57:41 -0400

    Added two mote ViewPatterns tests

---
+++ test/ghc/PatternSynonyms/T20485a.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE PatternSynonyms, MonoLocalBinds, TypeOperators #-}
+module T20485a where
+
+import Data.Type.Equality
+
+f :: a :~: b -> a -> b
+f Refl x = x
+
+pattern ReflPat = Refl
+
+g :: a :~: b -> a -> b
+g ReflPat x = x
