af7dda5f8640e1fd4364fa33f09ca374c5663ad8
Mario Blažević <blamario@protonmail.com>
2024-01-15 19:13:24 -0500

    GHC tests for QuantifiedConstraints

---
+++ test/ghc/QuantifiedConstraints/T16245.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+module T16245 where
+
+import Data.Kind
+
+type Const a b = a
+type SameKind (a :: k) (b :: k) = (() :: Constraint)
+class (forall (b :: k). SameKind a b) => C (k :: Const Type a)
