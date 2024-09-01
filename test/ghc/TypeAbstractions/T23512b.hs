9f91b5b0ecb6870485aa69e19f809b02eca843f5
Mario Blažević <blamario@protonmail.com>
2024-08-31 20:39:16 -0400

    Another TypeAbstractions test

---
+++ test/ghc/TypeAbstractions/T23512b.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies, TypeAbstractions #-}
+module T23512b where
+import GHC.Types
+
+type family F2 a :: k
+type instance F2 @(j -> j) Int = Any :: j -> j
+
+type family F3 a :: k
+type instance forall j. F3 Int = Any :: j -> j
